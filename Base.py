88c025fbce277ee3fc1515b84c74cc716ae81075
apexorange <117487377+apexorange@users.noreply.github.com>
2023-10-28 00:11:40 -0500

    making some progress

--- Base.py
+++ Base.py
@@ -8,11 +8,11 @@ class Base:
         self.return_results = 999
         self.image_width = 200
         self.all_categories = []
-        self.selected_categories = []
+        self.selected_categories = ["environmental"]
         self.active_keywords = []
-        self.selected_keywords = []
+        self.selected_keywords = ["cool"]
         self.logged_in = True
-        self.show_debug_form = False
+        self.show_debug_form_checked = False
         self.local_gallery = Importer.create_local_gallery()
 
     @property
@@ -24,28 +24,31 @@ class Base:
 
     def detect_active_form(self):
         if self.is_user_logged_in:
-            self.load_welcome()
-            if self.show_debug_form:
+            if self.show_debug_form_checked:
                 self.load_debug_form()
             else:
-                self.load_main()
+                self.load_main(self.selected_categories, self.selected_keywords)
         else:
             self.load_sign_in_page()
 
     # Startup Form Options
 
-    def load_sign_in_page(self):
-        pass
+    @staticmethod
+    def load_sign_in_page():
+        return print("sign-in page loaded")
 
-    def load_welcome(self):
-        pass
+    @staticmethod
+    def load_welcome():
+        return print("welcome page loaded")
 
-    def load_debug_form(self):
-        pass
+    @staticmethod
+    def load_debug_form():
+        return print("debug page loaded")
 
-    def load_main(self):
-        if Globals.selected_categories != [] or self.selected_keywords != []:
-            self.get_images_from_gallery(self.local_gallery, self.active_keywords, self.selected_categories)
+    def load_main(self, selected_categories: list, selected_keywords: list):
+        if not selected_categories and not selected_keywords:
+            print("gallery_loaded")
+            # self.get_images_from_gallery(self.local_gallery, self.active_keywords, self.selected_categories)
         else:
             self.load_welcome()
 
@@ -53,69 +56,71 @@ class Base:
 
     def initialize_and_display(self):
         # self.activate_and_load_keyword_autcomplete(Globals.active_keywords)
-        self.detect_active_form()
+        return self.detect_active_form()
 
-# GALLERY
+    """"""""""""""""""""""""""""""
+    """ GALLERY VIEW """
+    """"""""""""""""""""""""""""""
 
-    def get_images_from_gallery(self, gallery: list):
-        num_results = 1
-        for i, g in enumerate(gallery):
-            row_index = i
-            current_image = i
-            slide_title = g["Slide_Title"]
-            categories = g["Categories"]
-            keywords = g["Keywords"]
-            remote_path = g["Remote_Path"]
-            dropbox_url = g["Dropbox_Link"]
-
-        def anvil_assemble_gallery_items(self):
-            # What actually gets put onto the screen in anvil
-            link_item = []
-            flow_panel_1 = []
-
-            def add_image(item):
-                print("Image Component Added")
-                link_item.append(current_image)
-                # Link_item.add_component(Image(source=URLMedia(remote_path),  # FORM EVENT
-                #                               width=self.image_width,
-                #                               height=round(self.image_width * 9 / 16),
-                #                               spacing_above="small",
-                #                               background="theme:Gray 300",
-                #                               role="card")
-                #                         )
-
-            def add_slide_title(item):
-                print("Title Component Added")
-                link_item.append(slide_title)
-                # Link_item.add_component(Label(text=f'{current_image}. {slide_title}',  # FORM EVENT
-                #                               align="center",
-                #                               spacing_below="medium",
-                #                               spacing_above="None",
-                #                               width=self.image_width,
-                #                               foreground="black",
-                #                               role="subheading")
-                #                         )
-
-            def add_keywords(item):
-                link_item.append(keywords)
-                print("Keywords Component Added")
-                # Link_item.add_component(Label(text=f'{"".join(keywords)}',  # FORM EVENT
-                #                               font_size=14,
-                #                               foreground="theme:Gray 600",
-                #                               width=self.image_width,
-                #                               background='#efefef',
-                #                               align='center',
-                #                               spacing_above="none",
-                #                               spacing_below="medium")
-                #
-            self.flow_panel_1.append(link_item)
-            print(f'Number of results: {num_results}')
-        num_results += 1
+    # def get_images_from_gallery(self, gallery: list):
+    #     num_results = 1
+    #     for i, g in enumerate(gallery):
+    #         row_index = i
+    #         current_image = i
+    #         slide_title = g["Slide_Title"]
+    #         categories = g["Categories"]
+    #         keywords = g["Keywords"]
+    #         remote_path = g["Remote_Path"]
+    #         dropbox_url = g["Dropbox_Link"]
+    #
+    #     def anvil_assemble_gallery_items(self):
+    #         # What actually gets put onto the screen in anvil
+    #         link_item = []
+    #         flow_panel_1 = []
+    #
+    #         def add_image(item):
+    #             print("Image Component Added")
+    #             link_item.append(current_image)
+    #             # Link_item.add_component(Image(source=URLMedia(remote_path),  # FORM EVENT
+    #             #                               width=self.image_width,
+    #             #                               height=round(self.image_width * 9 / 16),
+    #             #                               spacing_above="small",
+    #             #                               background="theme:Gray 300",
+    #             #                               role="card")
+    #             #                         )
+    #
+    #         def add_slide_title(item):
+    #             print("Title Component Added")
+    #             link_item.append(slide_title)
+    #             # Link_item.add_component(Label(text=f'{current_image}. {slide_title}',  # FORM EVENT
+    #             #                               align="center",
+    #             #                               spacing_below="medium",
+    #             #                               spacing_above="None",
+    #             #                               width=self.image_width,
+    #             #                               foreground="black",
+    #             #                               role="subheading")
+    #             #                         )
+    #
+    #         def add_keywords(item):
+    #             link_item.append(keywords)
+    #             print("Keywords Component Added")
+    #             # Link_item.add_component(Label(text=f'{"".join(keywords)}',  # FORM EVENT
+    #             #                               font_size=14,
+    #             #                               foreground="theme:Gray 600",
+    #             #                               width=self.image_width,
+    #             #                               background='#efefef',
+    #             #                               align='center',
+    #             #                               spacing_above="none",
+    #             #                               spacing_below="medium")
+    #             #
+    #         self.flow_panel_1.append(link_item)
+    #         print(f'Number of results: {num_results}')
+    #     num_results += 1
 
 # Display to Anvil
 
-    def anvil_display_gallery_items(self):
-        print("Anvil output of gallery items")
+    # def anvil_display_gallery_items(self):
+        # print("Anvil output of gallery items")
         # Link_item = Link(row_spacing="small", col_spacing="small")  # FORM EVENT
         # # Check number of visible results from variable
         # if num_results <= self.return_results:
@@ -137,70 +142,70 @@ class Base:
         #     self.flow_panel_1.add_component(Link_item)
         #     self.label_results_number.text = f'Total Dataset: {num_results} Slides'
         #     num_results += 1
-        return None
+        # return None
 
     """"""""""""""""""""""""""""""
     """ SINGLE IMAGE VIEW """
     """"""""""""""""""""""""""""""
 
-    def image_view_builder(self):
-        self.column_panel_1.add_component(Image_View(image_num=self.current_clicked_image + 1,
-                                                     title=(
-                                                         self.local_gallery[self.current_clicked_image]["Slide_Title"]),
-                                                     image=URLMedia(self.local_gallery[self.current_clicked_image][
-                                                                        "Remote_Path"]),
-                                                     categories=(self.local_gallery[self.current_clicked_image][
-                                                         "Categories"]).title(),
-                                                     keywords=(
-                                                         self.local_gallery[self.current_clicked_image]["Keywords"]),
-                                                     dropbox_url=(self.local_gallery[self.current_clicked_image][
-                                                         "Dropbox_Link"])), full_width_row=True)
-
-    def image_click(self, **event_args):
-        """This method is called when the link is clicked"""
-        self.column_panel_1.clear()  # FORM EVENT
-        self.initialize_and_display()
-        self.current_clicked_image = event_args['sender'].tag[5]
-        self.image_view_builder()
-
-    def next_image_click(self):
-        self.column_panel_1.clear()  # FORM EVENT
-        if self.current_clicked_image < len(self.local_gallery) - 1:
-            self.current_clicked_image += 1
-            self.image_view_builder()
-        else:
-            self.initialize_and_display()
-
-    def prev_image_click(self):
-        self.column_panel_1.clear()  # FORM EVENT
-        if self.current_clicked_image > 0:
-            self.current_clicked_image -= 1
-            self.image_view_builder()
-        else:
-            self.initialize_and_display()
-
-    def refresh_selected_image(self):
-        self.column_panel_1.clear()  # FORM EVENT
-        # self.load_keywords_from_gallery(self.local_gallery)
-        self.image_view_builder()
-
-    def set_visibility(self, sel_cats, sel_keywords, panel):
-        panel_cats = panel.tag[1]
-        panel_keywords = panel.tag[2]
-
-        if sel_cats and not sel_keywords:
-            panel.visible = any(cat in panel_cats for cat in sel_cats)
-
-        elif sel_keywords and not sel_cats:
-            panel.visible = any(word in panel_keywords for word in sel_keywords)
-
-        elif sel_cats and sel_keywords:
-            panel.visible = any(word in panel_keywords for word in sel_keywords) and any(
-                cat in panel_cats for cat in sel_cats)
-
-        else:
-            panel.visible = False
+    # def image_view_builder(self):
+    #     self.column_panel_1.add_component(Image_View(image_num=self.current_clicked_image + 1,
+    #                                                  title=(
+    #                                                      self.local_gallery[self.current_clicked_image]["Slide_Title"]),
+    #                                                  image=URLMedia(self.local_gallery[self.current_clicked_image][
+    #                                                                     "Remote_Path"]),
+    #                                                  categories=(self.local_gallery[self.current_clicked_image][
+    #                                                      "Categories"]).title(),
+    #                                                  keywords=(
+    #                                                      self.local_gallery[self.current_clicked_image]["Keywords"]),
+    #                                                  dropbox_url=(self.local_gallery[self.current_clicked_image][
+    #                                                      "Dropbox_Link"])), full_width_row=True)
+    #
+    # def image_click(self, **event_args):
+    #     """This method is called when the link is clicked"""
+    #     self.column_panel_1.clear()  # FORM EVENT
+    #     self.initialize_and_display()
+    #     self.current_clicked_image = event_args['sender'].tag[5]
+    #     self.image_view_builder()
+    #
+    # def next_image_click(self):
+    #     self.column_panel_1.clear()  # FORM EVENT
+    #     if self.current_clicked_image < len(self.local_gallery) - 1:
+    #         self.current_clicked_image += 1
+    #         self.image_view_builder()
+    #     else:
+    #         self.initialize_and_display()
+    #
+    # def prev_image_click(self):
+    #     self.column_panel_1.clear()  # FORM EVENT
+    #     if self.current_clicked_image > 0:
+    #         self.current_clicked_image -= 1
+    #         self.image_view_builder()
+    #     else:
+    #         self.initialize_and_display()
+    #
+    # def refresh_selected_image(self):
+    #     self.column_panel_1.clear()  # FORM EVENT
+    #     # self.load_keywords_from_gallery(self.local_gallery)
+    #     self.image_view_builder()
+    #
+    # def set_visibility(self, sel_cats, sel_keywords, panel):
+    #     panel_cats = panel.tag[1]
+    #     panel_keywords = panel.tag[2]
+    #
+    #     if sel_cats and not sel_keywords:
+    #         panel.visible = any(cat in panel_cats for cat in sel_cats)
+    #
+    #     elif sel_keywords and not sel_cats:
+    #         panel.visible = any(word in panel_keywords for word in sel_keywords)
+    #
+    #     elif sel_cats and sel_keywords:
+    #         panel.visible = any(word in panel_keywords for word in sel_keywords) and any(
+    #             cat in panel_cats for cat in sel_cats)
+    #
+    #     else:
+    #         panel.visible = False
 
 
 base_form = Base()
-print(base_form.get_images_from_gallery(base_form.local_gallery))
+base_form.initialize_and_display()
