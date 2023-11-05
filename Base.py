9de73d5213abf7d4479ad5ef8c9fb0c080c6a30c
apexorange <117487377+apexorange@users.noreply.github.com>
2023-11-04 19:46:05 -0500

    working through reduction of items based on overlap of categories and keywords

--- Base.py
+++ Base.py
@@ -37,10 +37,9 @@ class Base:
     def choose_random_groups_of_keywords(all_keywords: list):
         random_keyword_list = []
         num_keywords = random.randint(0, 5)
-        for i, e in enumerate(all_keywords):
-            random_keyword_selector_seed = random.randint(0, len(all_keywords) - 1)
-            if len(random_keyword_list) < num_keywords:
-                random_keyword_list.append(all_keywords[random_keyword_selector_seed])
+        random_keyword_selector_seed = random.randint(0, len(all_keywords) - 1)
+        if len(random_keyword_list) < num_keywords:
+            random_keyword_list.append(all_keywords[random_keyword_selector_seed])
         return list(set(random_keyword_list))
 
     # Extract categories from local gallery
@@ -55,11 +54,10 @@ class Base:
     @staticmethod
     def choose_random_groups_of_categories(all_categories: list):
         random_category_list = []
-        num_categories = random.randint(0, 1)
-        for i, e in enumerate(all_categories):
-            random_category_selector_seed = random.randint(0, len(all_categories) - 1)
-            if len(random_category_list) < num_categories:
-                random_category_list.append(all_categories[random_category_selector_seed])
+        num_categories = random.randint(0, 3)
+        random_category_selector_seed = random.randint(0, len(all_categories) - 1)
+        if len(random_category_list) < num_categories:
+            random_category_list.append(all_categories[random_category_selector_seed])
         return random_category_list
 
     # Check login status
@@ -132,15 +130,14 @@ class Base:
 
 # Create gallery item
 
-class GalleryItem:
-    def __init__(self, slide_idx, title, categories, keywords):
-        self.slide_idx = slide_idx
+class GalleryItem():
+    def __init__(self, title, categories, keywords):
         self.title = title
         self.keywords = keywords
         self.categories = [categories]
 
     def __repr__(self):
-        return f'{int(self.slide_idx)}\n{self.title}\n{self.categories}\n{self.keywords}'
+        return f'{self.title}\n{self.categories}\n{self.keywords}'
 
 
 # Create instance of Base class
@@ -167,15 +164,31 @@ live_filter_lst: list = list(set(random_cats + random_keys))
 print(f'Active filters: {live_filter_lst}\n')
 
 
+def flatten_inner_lists(list_to_clean) -> list:
+    flattened_list = [item for sublist in list_to_clean for item in sublist]
+    return flattened_list
+
+
+def check_category_keyword_overlap():
+    category_test_lst = random_cats
+    active_keywords = []
+    for g in live_gallery:
+        if g["Categories"]:
+            if g["Categories"] in category_test_lst:
+                active_keywords.append(g["Keywords"])
+    return active_keywords
+
+
 def gallery_item_assembler():
     item_dict = {}
     for i, g in enumerate(live_gallery):
-        gallery_item = GalleryItem(g['Index'], g['Slide_Title'], g['Categories'], g['Keywords'])
+        gallery_item = GalleryItem(g['Slide_Title'], g['Categories'], g['Keywords'])
         for live_filter in live_filter_lst:
             if live_filter in g['Keywords'] and gallery_item not in item_dict:
                 item_dict.update({i: gallery_item})
     return item_dict
 
 
-result = gallery_item_assembler()
-print(result)
+output = flatten_inner_lists(check_category_keyword_overlap())
+
+print(output)
